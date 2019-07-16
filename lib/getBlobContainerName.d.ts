6958b7f8708018652116080fefa4e5dbbaae7de9
Matus Backor <MatusB@kentico.com>
2019-07-16 08:34:41 +0200

    KA-552 Exclude lib folder from GitHub

--- lib/getBlobContainerName.d.ts
+++
@@ -1,2 +0,0 @@
-import { IEventGridEvent } from './index';
-export declare const getBlobContainerName: <EventGridEventData>(eventGridEvent: IEventGridEvent<EventGridEventData>) => string;
--- lib/getRootCodenamesOfItem.d.ts
+++
@@ -1,3 +0,0 @@
-import { ContentItem } from 'kentico-cloud-delivery';
-import { IWebhookContentItem } from './index';
-export declare const getRootCodenamesOfItem: (item: IWebhookContentItem, allItems: ContentItem[], rootItemTypes: string[]) => string[];
